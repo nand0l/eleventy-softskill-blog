f6f6346a65d0c5b36446ad79b582ce76276022dc
Nando Lutgerink <nando.lutgerink@gmail.com>
2024-01-11 11:17:49 +0100

    replaced tabs with spaces

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,12 +1,12 @@
 module.exports = {
-	title: "SkillSoft Blog",
-	img: "./img/skillsoft-navy-logo.png",
-	url: "https://blog.skillsoft-demo.com",
-	language: "en",
-	description: "I am writing about my journey into the machine learning",
-	author: {
-		name: "Nando Lutgerink",
-		email: "nando.lutgerink+fromblog@gmail.com",
-		url: "https://portfolio.amazoninstructor.info/"
-	}
+    title: "SkillSoft Blog",
+    img: "./img/skillsoft-navy-logo.png",
+    url: "https://blog.skillsoft-demo.com",
+    language: "en",
+    description: "I am writing about my journey into the machine learning",
+    author: {
+        name: "Nando Lutgerink",
+        email: "nando.lutgerink+fromblog@gmail.com",
+        url: "https://portfolio.amazoninstructor.info/"
+    }
 }
